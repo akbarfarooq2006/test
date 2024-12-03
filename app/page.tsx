ee92ab518e9e50a8d44ab0865e6ca5a2c7df845c
akbar farooq <akbarfarooq2006@gmail.com>
2024-12-03 15:43:44 +0500

    third

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from "react";
-import Image from "next/image";
-import { count, log } from "console";
+
 
 export default function Home() {
 const [number, setnumber] = useState(0)  
